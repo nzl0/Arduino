e32e781954b3cd19b0752ff69774d57d079cf7d9
nzl0 <nazlicansoyer06@gmail.com>
2025-03-29 17:44:54 +0300

    check the tp press button with active buzzer

---
+++ Button_And_Buzzer/Button_And_Buzzer.ino
@@ -0,0 +1,24 @@
+int button1=13, button2=12, buzzer=2;
+int on=450,off=900;
+void setup() {
+  Serial.begin(9600);
+  pinMode(button1,INPUT);
+  pinMode(button2,INPUT);
+  pinMode(buzzer,OUTPUT);
+}
+
+void loop() {
+  if(digitalRead(button1)==HIGH){
+  	Serial.println("You are pressing button1. ");
+    digitalWrite(buzzer,HIGH);
+    delay(on);
+  }
+  else if(digitalRead(button2)==HIGH){
+  	Serial.println("You are pressing button2. ");
+    digitalWrite(buzzer,HIGH);
+    delay(on);
+  }
+  else{
+          	digitalWrite(buzzer,LOW);
+          }
+}
